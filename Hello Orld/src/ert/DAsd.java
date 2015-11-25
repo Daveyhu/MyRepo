04d25a4729c6b0c7a84a62b566ed2e27e908820f
Davey Hu <d.hu-1@student.utwente.nl>
2015-11-25 11:52:17 +0100

    Revert "Tweede Commit"
    This reverts commit 8f7b439841c4f6b9e9880201d2ed601b89bdb6ce.

--- Hello Orld/src/ert/DAsd.java
+++
@@ -1,5 +0,0 @@
-package ert;
-
-public class DAsd {
-
-}
--- Hello Orld/src/ert/Hello.java
+++ Hello Orld/src/ert/Hello.java
@@ -3,6 +3,5 @@ package ert;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("Mate");
 	}
 }
